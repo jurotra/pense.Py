0e4e94ac7712f46d7b9d729907cd2a7ed05bb1b4
Julia Trajano <juliartrajan0822@gmail.com>
2024-10-11 15:21:46 -0300

    renomeei as variaveis e traduzi o codigo

--- lista.py
+++ lista.py
@@ -1,31 +1,25 @@
 
 #lista de compras utilizando o for
-lista= []
+groceries= []
 while True:
 
-  pergunta= int(input("Vamos as compras! Quantos itens voce gostaria de comprar?"))
-  for i in range(pergunta):
-     item= input("Qual item voce gostaria de colocar?")
-     lista.append(item) #adicionar a lista
+  question= int(input("How many products would you like to buy?"))
+  for i in range(question):
+     product= input("Which product would you like to buy?")
+     groceries.append(product) #adicionar a lista
 
-  print("Esses são seus itens:")
-  for item in lista:
-     print(item)
+  print("These are your products:")
+  for product in groceries:
+     print(product)
  
  #adicionar mais itens
-  pergunta2= input("Gostaria de adicionar mais algum item?").strip().lower()
-  if pergunta2 != "sim":
-     print("Obrigada por comprar em nosso estabelecimento! Essa é a sua sacola de compras final:")
+  question2= input("Would you like to buy anything else?").strip().lower()
+  if question2 != "yes":
+     print("Thank you for shopping at our market! These are your products:")
      break
-  else:
-     pergunta3= int(input("Quantos itens voce gostaria de adicionar?"))
-     for i in range(pergunta3):
-       item= input("Qual item voce gostaria de colocar?")
-       lista.append(item) 
-     print("Obrigada por comprar em nosso estabelecimento! Essa é a sua sacola de compras final:")
-     for item in lista:
-           print(item)
-     break
+
+
+
 
 
 

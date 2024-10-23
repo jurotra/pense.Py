b89d59572ed77fbf58a66f6c2cdb3f5e6233ad50
Julia Trajano <juliartrajan0822@gmail.com>
2024-10-23 14:33:29 -0300

    lista é emitida no terminal

--- collatz.py
+++ collatz.py
@@ -9,17 +9,20 @@ def collatz_number(n):
         if n % 2==0: #even
             n = n // 2
             result.append(n)
-        elif n % 2!=0 or n!=1: #odd
+        elif n<=1:
+            break
+        else:
             n = n*3+1
             result.append(n)
-        else: 
-            break
-     
-    print(result)
+
+            
+        print(result)
     
 
 
 n = int(input("Enter a number to use in Collatz: "))
 collatz_number(n)
+
+
 
 
